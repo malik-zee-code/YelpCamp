d246bd3f2b62ea32f399fa08d799e7a697e4f02d
malik-zee-code <ze3063786@gmail.com>
2022-04-29 19:12:34 +0500

    Complete yelpcamp project

--- app.js
+++ app.js
@@ -9,18 +9,23 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ExpressError = require("./Utils/ExpressError");
 const engine = require("ejs-mate");
-const campgroundRoute = require("./Routes/campground");
-const reviewRoute = require("./Routes/review");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
-const userRoute = require("./Routes/user");
-const indexRoute = require("./Routes/index");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const MongoDBstore = require("connect-mongo");
+
+// ----------- Routes -----------
+
+const campgroundRoute = require("./Routes/campground");
+const reviewRoute = require("./Routes/review");
+const userRoute = require("./Routes/user");
+const indexRoute = require("./Routes/index");
+
+// ------------------------------
 
 //======================== HELMET CONFIG ====================================
 
@@ -76,7 +81,7 @@ app.use(
     crossOriginEmbedderPolicy: false,
   })
 );
-//======================== HELMET CONFIG ====================================
+//=====================================================================
 // process.env.DB_URL;
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 mongoose.connect(dbUrl);
@@ -130,9 +135,9 @@ app.use(flash());
 // - req.query
 app.use(mongoSanitize());
 
+//------------------some Configurations before using passport --------------------------
 app.use(passport.initialize());
 app.use(passport.session());
-//------------------some Configurations before using passport --------------------------
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -149,7 +154,7 @@ app.use("/campgrounds/:id/review", reviewRoute);
 app.use("/", userRoute);
 app.use("/", indexRoute);
 
-app.all("*", (req, res, next) => {
+app.all("*", (req, res) => {
   throw new ExpressError("Page Not Found", 404);
 });
 
